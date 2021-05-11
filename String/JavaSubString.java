820101962d5972b1c0950d6cace6f81955a5a3e6
Ankit Yadav <ankiii0071@gmail.com>
2021-05-12 04:59:07 +0530

    initial commit

---
+++ String/JavaSubString.java
@@ -0,0 +1,14 @@
+package strings;
+import java.util.*;
+public class JavaSubString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+	}
+
+}
