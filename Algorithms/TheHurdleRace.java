543fb27916a10637d54f7b44a47e4e02ca2256bf
Ankit Yadav <ankiii0071@gmail.com>
2021-05-22 03:05:59 +0530

    initial commit

---
+++ Algorithms/TheHurdleRace.java
@@ -0,0 +1,61 @@
+package algorithmsJava;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+public class TheHurdleRace {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> height = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.hurdleRace(k, height);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+
+	}
+	
+	static class Result {
+
+	    public static int hurdleRace(int k, List<Integer> height) 
+	    {
+	        Collections.sort(height);
+	        int max = Collections.max(height);
+	        int dose = 0;
+	        if(max>k)
+	        {
+	            dose = max-k;
+	        }
+	        else
+	        {
+	            dose =0;   
+	        }
+	        return dose;
+	    }
+
+	}
+}
