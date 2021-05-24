bb6bb34e7feb48b1f67aa969af9feac022f99021
Ankit Yadav <ankiii0071@gmail.com>
2021-05-24 23:35:05 +0530

    initial commit

---
+++ Algorithms/CompareTheTriplets.java
@@ -0,0 +1,66 @@
+package algorithmsJava;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class CompareTheTriplets {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+
+	}
+	static class Result {
+	    
+	    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) 
+	    {
+	        List<Integer> arr = new ArrayList<>();
+	        int bob =0;
+	        int alice =0;
+	        for(int i =0;i<a.size();i++)
+	        {
+	            if(a.get(i)>b.get(i))
+	            {
+	                alice++;
+	            }
+	            else if(a.get(i)<b.get(i))
+	            {
+	                bob++;
+	            }
+	          
+	        }
+	        arr.add(0,alice);
+	        arr.add(1,bob);
+	        return arr;
+	    }
+
+	}
+}
