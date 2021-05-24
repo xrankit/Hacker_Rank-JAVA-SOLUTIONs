7311bf2722f592315cff97467dab58130981bdef
Ankit Yadav <ankiii0071@gmail.com>
2021-05-24 23:34:10 +0530

    initial commit

---
+++ Algorithms/SimpleArraySum.java
@@ -0,0 +1,54 @@
+package algorithmsJava;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+public class SimpleArraySum {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+	}
+	
+	static class Result {
+
+	    /*
+	     * Complete the 'simpleArraySum' function below.
+	     *
+	     * The function is expected to return an INTEGER.
+	     * The function accepts INTEGER_ARRAY ar as parameter.
+	     */
+
+	    public static int simpleArraySum(List<Integer> ar) 
+	    {
+	        int sum =0;
+	        for(int i =0;i<ar.size();i++)
+	        {
+	            sum = sum + ar.get(i);
+	        }
+	        return sum;
+	    }
+
+	}
+}
