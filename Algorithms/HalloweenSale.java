3b7bdc5a53fafec6f07c5baa1792ae76e3a415fb
Ankit Yadav <ankiii0071@gmail.com>
2021-05-28 01:09:01 +0530

    initial commit

---
+++ Algorithms/HalloweenSale.java
@@ -0,0 +1,54 @@
+package algorithmsJava;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+public class HalloweenSale {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int p = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        int m = Integer.parseInt(firstMultipleInput[2]);
+
+        int s = Integer.parseInt(firstMultipleInput[3]);
+
+        int answer = Result.howManyGames(p, d, m, s);
+
+        bufferedWriter.write(String.valueOf(answer));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+		
+	}
+	static class Result {
+
+	    public static int howManyGames(int p, int d, int m, int s) 
+	    {
+	        int price = 0;
+	        while( s>=0)
+	        {
+	            s = s-Math.max(p, m);
+	            p = p - d;
+	            price++;
+	        } 
+	        return price-1;
+	        
+	    }
+
+	}
+}
